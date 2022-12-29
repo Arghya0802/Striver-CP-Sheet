3b989640a2d9fc0d53fdeaa3cc81d46cc047fb46
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2022-12-29 23:02:47 +0530

    Create Word Capitalization.cpp

---
+++ Word Capitalization.cpp
@@ -0,0 +1,27 @@
+// Link: https://codeforces.com/problemset/problem/281/A
+
+// Simple implementation problem
+
+#include <bits/stdc++.h>
+using namespace std ;
+ 
+int main(int argc, char const *argv[])
+{
+	string str ;
+	cin >> str ;
+ 
+	char first = str[0] ;
+ 
+	if(first >= 'a' && first <= 'z')
+	{
+		int val = (int) first ;
+ 
+		first = (char) (val - 32) ;
+ 
+		str[0] = first ;
+	}
+ 
+	cout << str << endl ;
+ 
+	return 0;
+}
