ccb07b82e931d0036e1daea700429ada0177d7d7
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2022-12-29 23:08:32 +0530

    Create Bear and Big Brother.cpp

---
+++ Bear and Big Brother.cpp
@@ -0,0 +1,38 @@
+// Link:
+
+// Again simple implementation problem
+
+#include <bits/stdc++.h>
+using namespace std ;
+ 
+int countYears(int a , int b)
+{
+	int cnt = 0 ;
+ 
+	while(a <= b)
+	{
+		a *= 3 ;
+ 
+		b *= 2 ;
+ 
+		cnt += 1 ;
+	}
+ 
+	return cnt ;
+}
+ 
+int main(int argc, char const *argv[])
+{
+	int a , b ;
+ 
+	cin >> a >> b ;
+ 
+	int ans = countYears(a , b) ;
+ 
+	cout << ans << endl ;
+ 
+	return 0;
+}
+
+// Time Complexity:  O(K) [No of Years required years]
+// Space Complexity: O(1)
