d89f40e0b9ccfd41ee6a583ee1c77a1467be1d99
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-01-01 19:03:36 +0530

    Create Repeating Cipher.cpp

---
+++ Repeating Cipher.cpp
@@ -0,0 +1,44 @@
+// Link:
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+// Basically we need to skip m-th element m number of times
+// Remember we are reverse mapping as well
+
+int main(int argc, char const *argv[])
+{
+	
+	int n ;
+	cin >> n ;
+
+	string s ;
+	cin >> s ;
+
+	string ans = "" ;
+
+	int cnt = 1 ;
+	int i = 0 ;
+
+	while(i < n)
+	{
+		ans += s[i] ;
+
+		int c = 0 ;
+
+		while(c < cnt) 
+		{
+			i++ ;
+			c++ ;
+		}
+
+		cnt += 1 ;
+	}
+
+	cout << ans << endl ;
+
+	return 0;
+}
+
+// Time Complexity:  O(N)
+// Space Complexity: O(N) [To store the final answer string]
